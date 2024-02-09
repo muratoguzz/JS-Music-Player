131975516ca3ba1aa694f8c0cbc7a1976e477aeb
Murat Oğuz <151613636+muratoguzz@users.noreply.github.com>
2024-02-09 15:14:14 +0300

    Update app.js

--- app.js
+++ app.js
@@ -133,7 +133,6 @@ volumeBar.addEventListener("input", (e) => {
 });
 
 //Müzik seçme ve başlatma
-
 const displayMusicList = (list) => {
     for(let i=0; i< list.length; i++){
         let liTag =`
